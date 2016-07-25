554af7cd8def13f400200eb1b6f1106e4a957499
quinn longhurst <qlonghurst@gmail.com>
2016-07-25 12:10:30 -0700

    turn it in twice

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
+
+
+
+
+
+
 //Problem: User when clicking on image goes to a dead end
 //Solution: Create an overlay with the large image - Lightbox
+
 
 var $overlay = $('<div id="overlay"></div>');
 var $image = $("<img>");
